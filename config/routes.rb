4ab691533cb6ffce1215984b594fb315631b9b7a
yuuhei.koutoku <csyu15095@g.nihon-u.ac.jp>
2020-11-01 12:25:59 +0000

    technologies controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'technologies/index'
+  get 'technologies/show'
+  get 'technologies/new'
+  get 'technologies/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
