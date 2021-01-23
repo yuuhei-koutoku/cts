de867223fb0fec7d91fd4a58ef3786fc3198596e
yuuhei.koutoku <csyu15095@g.nihon-u.ac.jp>
2021-01-23 02:50:56 +0000

    s3-edit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # 本番環境(production)の保存先を:localから:amazonに変更
+  config.active_storage.service = :amazon
 end
