bdde04b8e1435ed46f3b8baa0b0457096855aa74
yuuhei.koutoku <csyu15095@g.nihon-u.ac.jp>
2021-01-23 07:42:35 +0000

    annotate-install

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id            :bigint           not null, primary key
+#  content       :text(65535)
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  technology_id :bigint
+#  user_id       :bigint
+#
+# Indexes
+#
+#  index_comments_on_technology_id  (technology_id)
+#  index_comments_on_user_id        (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (technology_id => technologies.id)
+#  fk_rails_...  (user_id => users.id)
+#
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :technology
--- app/models/technology.rb
+++ app/models/technology.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: technologies
+#
+#  id         :bigint           not null, primary key
+#  content    :text(65535)
+#  title      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_technologies_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class Technology < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
